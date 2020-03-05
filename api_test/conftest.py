449e4c28e9338acab2402ac8b74a4882b11ec2da
unknown <1014422757@qq.com>
2020-03-05 14:35:36 +0800

    增加切换环境参数化

--- api_test/conftest.py
+++ api_test/conftest.py
@@ -1,11 +1,20 @@
 import pytest
 import os
 from Environment import Env
+from configparser import ConfigParser
+
 
 @pytest.fixture(scope = "module",autouse =True)
 def env():
-    yield Env(username=os.environ["username"],password=os.environ["password"])
+    config = ConfigParser()
+    config.read(["data.ini"])
+    # 1.使用环境变量读取的方式，需要吧evn设置为 data.ini下的环境名,并将"env":"test_env"设置到环境变量中
+    api_root_url = config[os.environ['env']]['api_root_url']
+    # 2.直接读取
+    # api_root_url = config['test_env']['api_root_url']
+    yield Env(api_root_url = api_root_url,username=os.environ["username"],password=os.environ["password"])
     # yield Env(token=os.environ["token"])
+
 
 @pytest.fixture(scope = "module", autouse=True)
 def just_print():
--- libraries/Environment.py
+++ libraries/Environment.py
@@ -2,6 +2,7 @@ from github import Github
 
 
 class Env:
-    def __init__(self,  username,password):
-        self.github = Github("https://api.github.com", username=username,password =password)
+    def __init__(self,api_root_url, username, password):
+        # self.github = Github("https://api.github.com", username=username,password =password)
         # self.github = Github("https://api.github.com", token=token)
+        self.github = Github(api_root_url, username =username, password =password)
